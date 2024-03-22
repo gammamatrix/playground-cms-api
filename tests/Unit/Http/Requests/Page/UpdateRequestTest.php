33a21952a72b6ca8db2fddfdde0ef773f8f09ced
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-03-22 15:10:56 -0700

    GH-1

---
+++ tests/Unit/Http/Requests/Page/UpdateRequestTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Playground
+ */
+namespace Tests\Unit\Playground\Cms\Api\Http\Requests\Page;
+
+use Playground\Cms\Api\Http\Requests\Page\UpdateRequest;
+use Tests\Unit\Playground\Cms\Api\Http\Requests\RequestTestCase;
+
+/**
+ * \Tests\Unit\Playground\Cms\Api\Http\Requests\Page\UpdateRequestTest
+ */
+class UpdateRequestTest extends RequestTestCase
+{
+    protected string $requestClass = UpdateRequest::class;
+
+    public function test_UpdateRequest_rules_with_optional_revisions_disabled(): void
+    {
+        config(['playground-cms-api.revisions.optional' => false]);
+        $instance = new UpdateRequest;
+        $rules = $instance->rules();
+        $this->assertNotEmpty($rules);
+        $this->assertIsArray($rules);
+        $this->assertArrayNotHasKey('revision', $rules);
+    }
+
+    public function test_UpdateRequest_rules_with_optional_revisions_enabled(): void
+    {
+        config(['playground-cms-api.revisions.optional' => true]);
+        $instance = new UpdateRequest;
+        $rules = $instance->rules();
+        $this->assertNotEmpty($rules);
+        $this->assertIsArray($rules);
+        $this->assertArrayHasKey('revision', $rules);
+        $this->assertSame('bool', $rules['revision']);
+    }
+}
